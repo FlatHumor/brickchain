a225fb4ae9527bc1116e219695713e64dc2ff361
FlatHumor <eatenbrains@gmail.com>
2018-12-15 15:33:34 +0300

    some methods of Chain moved to private section

--- chain.h
+++ chain.h
@@ -22,18 +22,19 @@ class Chain
     const std::string DEFAULT_HASH = "E175E69A8312A66A7011C1DCCFC2CF69823BC2C52F58CA516F60698F02DAA1D6";
 public:
     explicit Chain(std::string &);
+    void add_transaction(Transaction &);
+    bool validate();
+
+private:
     static std::string build_hash(std::string);
     static bool check_nonce(std::string, int32_t, std::string);
     static void calculate_nonce(Brick *);
     void get_previous_brick(Brick *);
-    void add_transaction(Transaction &);
-    std::vector<std::string> get_bricks_filenames();
-
-private:
     void save_brick(Brick &, std::string &);
     void load_brick(Brick *, std::string &);
     static std::string increment_filename(std::string &);
     const std::string bricks_path;
+    std::vector<std::string> get_bricks_filenames();
 };
 
 
